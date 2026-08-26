cb4357808fe21586b7a0225df144ec29f913b7c2
Victor Passos <victorerycp@gmail.com>
2026-08-26 17:23:42 -0300

    Create Malado.py

---
+++ Malado.py
@@ -0,0 +1,178 @@
+
+while True:
+    print()
+    print("O MEGA ATAQUE DOS 67 PINGUINS")
+    print()
+    print("Deseja jogar? <sim><não>")
+    iniciar=input("")
+    if iniciar.lower()=="sim":
+        print(" Papoi, Ubaldo, Pitufo e Astrogildo estavam na confretarnização no curso de Python, até que o professorzinho Victorzinho Passinhos decide  se rebelar contra seus alunos e revela seu plano medonho e extinguir a vida humana e governar um mundo formado apenas por pinguins, junto de seus aliados Osvalda e Gargamel.")
+        print()
+        print("Agora o quarteto do balacobaco terá que enfrentar Victorzinho (the goat) para salvar a humanidade")
+        print()
+        print()
+        print("Personagens")
+        print("*1 - Papoi | força = 1900*")
+        print("*2 - Ubaldo vaqueiro | força = 1900*")
+        print("*3 - Papa Pitufo | força = 1800*")
+        print("*4 - Astrogildo | força = 2100*")
+        pe=input("Escolha um personagem e insira o numero dele: ")
+        if pe=="1":
+            fp=1900
+            np="Papoi"
+        elif pe=="2":
+            fp=1900
+            np="Ubaldo Vaqueiro"
+        elif pe=="3":
+            fp=1800
+            np="Pitufo"
+        elif pe=="4":
+            fp=2100
+            np="Astrogildo"
+        else:
+            print("Escolha um dos personagens, ser de capacidade intelectual limitada")
+            continue
+        print()
+        print("Armas")
+        print("1 - Faca de pão | força = 500")
+        print("2 - Makita | força = 800")
+        print("3 - Chinelo da mamãe | força = 900")
+        print("4 - Detector de IA | força = 600")
+        ae=input("Escolha uma arma e insira o numero dela: ")
+        if ae=="1":
+            fa=500
+            na="Faca de pão"
+        elif ae=="2":
+            fa=800
+            na="Makita"
+        elif ae=="3":
+            fa=900
+            na="Chinelo da mamãe"
+        elif ae=="4":
+            fa=600
+            na="Detector de IA"
+        else:
+            print("Escolha uma das armas acima, animal🐱‍🐉")
+            continue
+        print()
+        ft=fp+fa
+        print(f"Sua força total é {fp+fa} senior calabreso✌😜")
+        print()
+        print("Seu primeiro oponente será: GARGAMEL")
+        print()
+        print("Utilizando de sua incrivel IA GENERATIVA ele cola seus ataques para te enfrentar!")
+        print()
+        fg=2500
+        print(f"A força do seu oponente é {fg}")
+        print()
+        print("hora do ataque")
+        print()
+        if ft>=fg:
+            print (f"Com toda sua força, {np} consegue derrotar a IA de Gargamel ultilizando {na}!")
+                
+        elif ft<fg:
+            print(f"{np} perdeu por SUA causa, sinta-se culpado para o resto da eternidade ser desprovido de intelecto funcional 🐧💀💀💀💣💥")
+            continue
+        print()
+        print("digite '67' para continuar")
+        continuar=input("")
+        if continuar=="67":
+            print()
+            print("Sua segunda oponente será: OSVALDA")
+            print()
+            print("Com sua mega inteligencia e seu 0 de carisma, ela utilizará todos os carros que ela não vendeu para te atingir")
+            print("agora você vai escolher outra arma para o proximo ataque")
+            print("Armas")
+            print("1 - Fitinha da copa | força = 1100")
+            print("2 - Galopá | força = 1200")
+            print("3 - Carisma | força = 1500")
+            print("4 - Intelectualidade | força = 1300")
+            ae2=input("Escolha uma arma e insira o numero dela: ")
+            if ae2=="1":
+                fa2=1100
+                na2="Fitinha da copa"
+            elif ae2=="2":
+                fa2=1200
+                na2="Galopá"
+            elif ae2=="3":
+                fa2=1500
+                na2="Carisma"
+            elif ae2=="4":
+                fa2=1300
+                na2="Intelectualidade"
+            else:
+                print("Escolha uma das armas criatura👀👊🏻")
+                continue
+            print()
+            ft2=fp+fa2
+            print(f"Sua força total é {ft2}")
+            print()
+            fo=3330
+            print(f"A força do seu oponente é {fo}")
+            print()
+            print("hora do ataque")
+            print()
+            if ft2>=fo:
+                print (f"Como {np} é uma boa pessoa, usando {na2}, ele consegue vender mais carros que ela, assim a derrotando-a ") 
+            elif ft2<fo:
+                print(f"{np} perdeu por SUA causa, sinta-se culpado para o resto da eternidade ser desprovido de intelecto funcional 🐧💀💀💀💣💥")
+                continue
+            print()
+        else:
+            print("vai voltar ao inicio automaticamente")
+            continue
+        print()
+        print("digite '67' para continuar")
+        continuar2=input("")
+        if continuar2=="67":
+            print()
+            print("Seu ÚLTIMO oponente é: VICTORZINHO PASSINHOS (ai meu Deus q medonho)")
+            print()
+            print(f"Com seu exercito de pinguins e medo de pessoas, Vicinho ataca {np} com toda sua furia!")
+            print()
+            print("agora você vai escolher outra arma para o proximo ataque")
+            print("Armas")
+            print("1 - Veja/Azulin | força = 2500")
+            print("2 - força do sono que faz os objetos deitarem | força = 2800")
+            print("3 - LOL | força = 3000")
+            print("4 - Verde | força = 2900")
+            ae3=input("Escolha uma arma e insira o numero dela: ")
+            if ae3=="1":
+                fa3=2500
+                na3="Veja/Azulin"
+            elif ae3=="2":
+                fa3=2800
+                na3="Força do sono que faz os objetos deitarem"
+            elif ae3=="3":
+                fa3=3000
+                na3="LOL"
+            elif ae3=="4":
+                fa3=2900
+                na3="Verde"
+            else:
+                print("É para escolher uma das armas né😔💢")
+                continue
+            print()
+            ft3=fp+fa3
+            print(f"Sua força total é {ft3}")
+            print()
+            fv=3330
+            print(f"A força do seu oponente é {fv}")
+            print()
+            print("hora do ataque")
+            print()
+            if ft2>=fv:
+                print (f"Com {na3}, {np} atinge Victorzinho (não mais) Passinhos de forma malada,salvando assim a humanidade") 
+                print("PARABÉNS 😱👏⭐ (n fez mais q sua obrigação)")
+            elif ft2<fv:
+                print(f"{np} perdeu por SUA causa, sinta-se culpado para o resto da eternidade ser desprovido de intelecto funcional 🐧💀💀💀💣💥")
+                continue
+        else:
+            print("vai voltar ao inicio automaticamente")
+            continue
+    elif iniciar.lower()=="não" or "nao":
+        print("Tá aqui porque então?? melhore🙄🖐")
+        break
+    else:
+        print("escolha sim ou não, que dificuldade tem isso?")
+        
